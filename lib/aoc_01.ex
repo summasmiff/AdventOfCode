f69b6b02f6384fd85f9c8025d0614b90e24a94a3
Summer Smith <jessica.summer.smith@gmail.com>
2018-12-04 12:45:09 -0500

    day 1 part 2

--- lib/aoc_01.ex
+++ lib/aoc_01.ex
@@ -1,15 +1,26 @@
 defmodule AdventOfCode.Day1 do
-  def calculate(acc, []), do: acc
-  def calculate(acc, [""]), do: acc
-  def calculate(acc, [ head | list ]) do
-    calculate(acc + String.to_integer(head), list)
+  def calculate_pt1(acc, []), do: acc
+  def calculate_pt1(acc, [""]), do: acc
+  def calculate_pt1(acc, [ head | list ]) do
+    calculate_pt1(acc + String.to_integer(head), list)
+  end
+
+  def calculate_pt2(acc, [""], freqList, listCopy), do: calculate_pt2(acc, listCopy, freqList, listCopy)
+  def calculate_pt2(acc, [ head | list ], [], listCopy) do
+    calculate_pt2(acc + String.to_integer(head), list, [acc], listCopy)
+  end
+  def calculate_pt2(acc, [ head | list ], freqList, listCopy) do
+    if Enum.member?(freqList, acc) do
+      acc
+    else
+      calculate_pt2(acc + String.to_integer(head), list, [acc | freqList], listCopy)
+    end
   end
 
   def main(args) do
     list = File.read!(args)
     |> String.split("\n")
 
-    total = calculate(0, list)
-    IO.puts(inspect(total))
+    IO.puts("pt 1: #{inspect(calculate_pt1(0, list))}. pt 2: #{inspect(calculate_pt2(0, list, [], list))}")
   end
 end
