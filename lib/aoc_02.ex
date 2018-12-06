877cdc2210a8630884af0c26abf5a9e7cccc4634
Summer Smith <jessica.summer.smith@gmail.com>
2018-12-06 10:08:50 -0500

    day 2 part 2

--- lib/aoc_02.ex
+++ lib/aoc_02.ex
@@ -56,11 +56,57 @@ defmodule AdventOfCode.Day2 do
     calc(list, checksum_list)
   end
 
+  def compare_words([], _, list), do: list
+  def compare_words(_, [], list), do: list
+  def compare_words([first_h | first_t], [second_h | second_t], list) do
+    # if it matches the same position in word2, put it in the array
+    list = if (first_h == second_h) do
+      [first_h | list]
+    else
+      list
+    end
+
+    compare_words(first_t, second_t, list)
+  end
+
+  def compare_against_list(_, [], list1), do: list1
+  def compare_against_list(word, [h | t], list1) do
+    word2 = String.graphemes(h)
+    list = compare_words(word, word2, [])
+
+    cond do
+      length(list) > length(list1) -> compare_against_list(word, t, list)
+      true -> compare_against_list(word, t, list1)
+    end
+  end
+
+  def calc2([h | []], list1) do
+    IO.puts("b")
+    word1 = String.graphemes(h)
+
+    list = compare_words(word1, list1, [])
+    cond do
+      length(list) > length(list1) -> list
+      true -> list1
+    end
+  end
+  def calc2([h | tail], list1) do
+    IO.puts("a")
+    word1 = String.graphemes(h)
+    list = compare_against_list(word1, tail, list1)
+
+    cond do
+      length(list) > length(list1) -> calc2(tail, list)
+      true -> calc2(tail, list1)
+    end
+  end
+
   def main(args) do
     list = File.read!(args)
     |> String.trim()
     |> String.split("\n")
 
-    IO.puts(calc(list, [0, 0]))
+    # IO.puts(calc(list, [0, 0]))
+    IO.puts(Enum.reverse(calc2(list, [])))
   end
 end
